8dd8118ed10c8e95858c66b7dc485b31f2bb5116
91636 <91636@INBOOK_X1_SLIM>
2024-07-23 13:33:33 +0530

    Comparator

---
+++ b96corejava/src/com/darpana/assignment/employees/DeveloperextendsEmployee.java
@@ -0,0 +1,18 @@
+package com.darpana.assignment.employees;
+
+public class DeveloperextendsEmployee extends Employee {
+    private String programmingLanguage;
+
+    public DeveloperextendsEmployee(String name, int employeeId, double salary, String programmingLanguage) {
+        super(name, employeeId, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+}
